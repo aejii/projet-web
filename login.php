3112422c7d017cb88887233d7fa292903b490dc5
pierre <pierrecadot44@gmail.com>
2019-04-08 11:39:16 +0200

    wip
    redirection index->login si non connecter

--- login.php
+++ login.php
@@ -24,49 +24,41 @@
         // $stats = new Statistiques();
         
         if (!isset($_SESSION['connecte'])) {
-            //ok
-        
-    ?>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4 col-md-offset-4">
-                <div class="login-panel panel panel-default">
-                    <div class="panel-heading">
-                        <h3 class="panel-title">Se connecter</h3>
-                    </div>
-                    <div class="panel-body">
-                        <form role="form"action="login.php" method = "post">
-                            <fieldset>
-                                <div class="form-group">
-                                    <input class="form-control" placeholder="E-mail" name="login" autofocus>
-                                </div>
-                                <div class="form-group">
-                                    <input class="form-control" placeholder="Mot de passe" name="mdp" type="password" value="">
-                                </div>
-                                <input type="submit" class="btn btn-lg btn-success btn-block" value="Valider">
-                                <a href="mdpoublier.php" class="active">Mots de passe oublié ?</a>
-                                <div class="form-group">
-                                    <a href="inscription.php" class="active">Pas de compte ? S'inscrire</a>
-                                </div>
-                            </fieldset>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+            //ok on se connecte
+            require "formulaireconnexion.php" ;
+        } else if ((isset($_POST["pseudo"]))&&(isset($_POST["mdp"])))
+        {
+            //On se connecte à la base de données et on vérifie les informations
+            //Si l'authentification est OK, on créé les variables de session ID et pseudo en leur assignant l'id de l'utilisateur et son pseudo
 
+            //Sinon, on affiche un message d'erreur suivi du formulaire d'authentification
+            $bdd = new PDO("mysql:host=localhost;dbname=phpesiea", "root", "L0t7vBQTNF94tQCq7J");
+            $req = $bdd->prepare("SELECT ID FROM Utilisateurs WHERE pseudo=:pseudo AND empreinte=:empreinte");
+            $req->execute(array(
+            "pseudo" => $_POST["pseudo"],
+            "empreinte" => hash("sha256", "jaimelesbananes".$_POST["mdp"])
+            ));
+
+            //while ( $donnees = $req->fetch() )
+            $donnees = $req->fetch();
+            if ($donnees) {	
+                $_SESSION['pseudo'] = $_POST["pseudo"];
+                $_SESSION["ID"] = $donnees['ID'];
+                header('Location: index.php');
+            }else{
+                echo "mdp incorrect";
+                require "formulaireconnexion.php" ;
+            }
+        } else {
+            //echo hash("sha512", "jaimelesbananes"."supersayan"."jaimelesbananes");
+            header('Location: index.php');
+        }
+    ?>
+    
     <script src="vendor/jquery/jquery.min.js"></script>
     <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
     <script src="vendor/metisMenu/metisMenu.min.js"></script>
     <script src="dist/js/sb-admin-2.js"></script>
-
-    <?php
-        } else {
-            //echo hash("sha512", "jaimelesbananes"."supersayan"."jaimelesbananes");
-            // header('Location: aejii.php');
-        }
-    ?>
 </body>
 
 </html>
