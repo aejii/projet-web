327e8b7494d6d4d7438665f3a45add0e5fcc02af
pierre <pierrecadot44@gmail.com>
2019-04-12 13:47:57 +0200

    v1
    validation local en js

--- formulaireinscription.php
+++ formulaireinscription.php
@@ -6,19 +6,19 @@
                     <h3 class="panel-title">S'inscrire</h3>
                 </div>
                 <div class="panel-body">
-                    <form role="form"action="inscription.php" enctype ="multipart/form-data" method = "post">
+                    <form role="form" name="inscriptform" action="inscription.php" enctype ="multipart/form-data" method = "post" onsubmit="return validateForm()">
                         <fieldset>
                             <div class="form-group">
-                                <input class="form-control" placeholder="Nom d'utilisateur" name="pseudo" autofocus>
+                                <input class="form-control" placeholder="Nom d'utilisateur" id="pseudo" name="pseudo" autofocus>
                             </div>
                             <div class="form-group">
-                                <input class="form-control" placeholder="Email" name="email" autofocus>
+                                <input class="form-control" placeholder="Email" id="email" name="email" autofocus>
                             </div>
                             <div class="form-group">
-                                <input class="form-control" placeholder="Mot de passe" name="mdp" type="password" value="">
+                                <input class="form-control" placeholder="Mot de passe" id="mdp" name="mdp" type="password" value="">
                             </div>
                             <div class="form-group">
-                                <input class="form-control" placeholder="Confirmer mot de passe" name="cmdp" type="password" value="">
+                                <input class="form-control" placeholder="Confirmer mot de passe" id="cmdp" name="cmdp" type="password" value="">
                             </div>
                             <div class="form-group">
                             	Photo de profil : 
@@ -43,3 +43,35 @@
         </div>
     </div>
 </div>
+
+<script>
+    function validateForm() {
+        var x = document.forms["inscriptform"]["pseudo"].value;
+        if (x.length <= 4) {
+            alert("Votre pseudo doit possèder au moins 5 caractères");
+            return false;
+        }
+        const regex = new RegExp(/([^A-Zéçàèêëîïa-z0-9-])/);
+        if (regex.test (x)) {
+            alert("Votre pseudo doit possèder seulement des caractères a-z 0-p çéèàêîïë");
+            return false;
+        }
+        var a = document.forms["inscriptform"]["email"].value;
+        const regexEmail = new RegExp(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+        if (!regexEmail.test(a)) {
+            alert("Votre email semble faux :/");
+            return false;
+        }
+        var y = document.forms["inscriptform"]["mdp"].value;
+        var z = document.forms["inscriptform"]["cmdp"].value;
+        if (y.length <= 8) {
+            alert("Votre mot de passe doit possèder au moins 8 caractères");
+            return false;
+        }
+        if (y != z) {
+            alert("Votre mot de passe de confirmation est faux :O");
+            return false;
+        }
+        return true;
+    }
+</script>
