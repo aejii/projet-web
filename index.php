94cc3fe89f17c15efa84528d36ca193f761984ca
Wolff <octivian@hotmail.fr>
2019-04-26 09:33:48 +0200

    Add update() function

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8" />
     <link rel="stylesheet" href="style.css" />
     <title>WaifuClicker</title>
+	<script type="text/javascript" src="script.js" ></script>
 </head>
 
 <header>
@@ -227,8 +228,8 @@
     </div>
 
     <div id="center">
-        <hearts>5624 </hearts><img id="heart" src="Images/heart.png"/>
-        <img id="clicMe" class="pulse" src="Images/clicMe.png" />
+        <hearts id="score">0 </hearts><img id="heart" src="Images/heart.png"/>
+        <img id="clicMe" class="pulse" src="Images/clicMe.png" onclick="click()" />
     </div>
 
 </body>
--- script.js
+++ script.js
@@ -3,14 +3,23 @@ var score = 0; // Counts the player click and the bonuses
 
 
 /*////////////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+| update: Update the score output in the html page							|
+|																			|
+| returns: void																|
+\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\////////////////////////////////////*/
+function update()
+{
+	document.getElementById("score").innerHTML = score;
+}
+
+
+/*////////////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 | click: Increase the score by 1 when the player clicks on the main button	|
 |																			|
 | returns: void																|
 \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\////////////////////////////////////*/
 function click()
 {
-	score++;
+	++score;
+	update();
 }
-
-
-
