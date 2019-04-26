319ed745105f71a94bfcad27ecdacb9869148eb8
Wolff <octivian@hotmail.fr>
2019-04-26 09:16:11 +0200

    Javascript script creation

---
+++ script.js
@@ -0,0 +1,16 @@
+/* Variable declaration */
+var score = 0; // Counts the player click and the bonuses
+
+
+/*////////////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+| click: Increase the score by 1 when the player clicks on the main button	|
+|																			|
+| returns: void																|
+\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\////////////////////////////////////*/
+function click()
+{
+	score++;
+}
+
+
+
