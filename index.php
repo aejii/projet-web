ee8c4ff43ae1acdcd783045683fffed607c63cce
pierre <pierrecadot44@gmail.com>
2019-05-28 14:16:40 +0200

    script.js qui reviens

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
     <link href="dist/css/sb-admin-2.css" rel="stylesheet">
     <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="style.css" />
+    <script type="text/javascript" src="script.js" ></script>
 </head>
 
 <header>
