aeceb24a5157fb99c744e8098b3734577783381d
Unknown <pierrecadot44@gmail.com>
2019-05-29 22:39:52 +0200

    correction profil par pierre

--- profil.php
+++ profil.php
@@ -1,11 +1,6 @@
 <?php 
 session_start();
-$bdd = new PDO("mysql:host=localhost;dbname=projetweb", "root", ""); 
-$serveur = "mysql";
-$host = "localhost";
-$base = "projetweb";
-$user = "root";
-$pass = "";
+require "connection.php";
 
 if(isset($_GET['id']) AND $_GET['id']>0)
 {
@@ -15,20 +10,6 @@ if(isset($_GET['id']) AND $_GET['id']>0)
 
     $userinfo = $requser->fetch(); // pour chcercher les information de l'utilisateurs 
 }
-
-//requete de "toute" les pages
-$loginrequete = "SELECT ID FROM utilisateurs WHERE pseudo=:pseudo AND empreinte=:empreinte";
-
-$wherepseudo = "SELECT ID FROM utilisateurs WHERE pseudo=:pseudo";
-$creationuser = "INSERT INTO utilisateurs (`pseudo`,`empreinte`,`inscription`,`image`,`role` , `ipinscription`, `lienvalidation`, `email`, `resetmdp`) VALUES (:pseudo , :empreinte , :insciption , :image ,1, :ip, :lienvalidation, :email, :resetmdp )";
-
-$newmessage = "INSERT INTO messages (`id_utilisateur`,`id_discussion`,`creation` , `contenu`) VALUES (:id_utilisateur , :id_discussion , :creation, :contenu )";
-$affichageconversation = "SELECT utilisateurs.pseudo AS pseudo, utilisateurs.image AS image, messages.creation AS creation, messages.contenu AS contenu FROM messages INNER JOIN utilisateurs ON Messages.ID_Utilisateur = Utilisateurs.ID WHERE messages.ID_Discussion=:id ORDER BY messages.creation ASC";
-$creationdiscussion = "INSERT INTO discussions (`titre`,`creation`,`auteur`) VALUES (:titre , :creation , :auteur )";
-$afficherlistediscussion = "SELECT id_discussion, TITRE, CREATION, AUTEUR FROM discussions";
-$nbrmsgenvoyer = "SELECT COUNT(ID) AS nbMessages FROM messages WHERE ID_Discussion=:id";
-$derniermsgenvoyer = "SELECT MAX(creation) AS dateMaximum FROM messages WHERE ID_Discussion=:id";
-
 
 if(isset($_FILES['avatar']) AND !empty($_FILES['avatar']['name']))
 {
